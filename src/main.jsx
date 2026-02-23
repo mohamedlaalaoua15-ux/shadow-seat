054e65b0c1cf519ff801fbc6625ef4c188e06038
Meedy <mohamedlaalaoua15@gmail.com>
2026-02-23 06:39:15 +0000

    Create src/main.jsx as entry point for React app

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(\n  <React.StrictMode>\n    <h1>Hello, World!</h1>\n  </React.StrictMode>\n);
