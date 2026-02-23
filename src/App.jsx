9ed9e96e233723d260c408236f829eada1229530
Meedy <mohamedlaalaoua15@gmail.com>
2026-02-23 06:39:26 +0000

    Create src/App.jsx file for Shadow Seat application

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Shadow Seat</h1>
+      <p>Your seat reservation system.</p>
+    </div>
+  );
+};
+
+export default App;
